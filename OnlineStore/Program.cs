1f6a3fde16a2bc8f63c32befa112c06add46dba6
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-06-05 12:01:06 +0300

    Created the Admin area with the Dashboard controller with the Index view and the AdminLayout view for the admin layout page.Configure the main logic that will be used for the users(User and Admin) in the app and creating the corresponding cshtml for the visualization of this features.

--- OnlineStore/Program.cs
+++ OnlineStore/Program.cs
@@ -67,6 +67,10 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+	  name: "areas",
+	  pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Product}/{action=Index}/{id?}");
 
