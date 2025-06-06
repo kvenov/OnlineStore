e9fc2a7dc3ed05f6d0194e5e607f1d2b0f8a59a6
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-06-06 14:34:08 +0300

    Modified the Identity Register.cshtml.cs so when a User is registered via the Identity UI, a new User role to be asigned for him.This way we ensure that for every new created user a User role is going to be asigned for him.

--- OnlineStore/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlineStore/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -121,19 +121,21 @@ namespace OnlineStore.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
-                    //var userId = await _userManager.GetUserIdAsync(user);
-                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    //code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                    //var callbackUrl = Url.Page(
-                    //    "/Account/ConfirmEmail",
-                    //    pageHandler: null,
-                    //    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
-                    //    protocol: Request.Scheme);
+                    await this._userManager.AddToRoleAsync(user, "User");
 
-                    //await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    //    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+					//var userId = await _userManager.GetUserIdAsync(user);
+					//var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+					//code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+					//var callbackUrl = Url.Page(
+					//    "/Account/ConfirmEmail",
+					//    pageHandler: null,
+					//    values: new { area = "Identity", userId = userId, code = code, returnUrl = returnUrl },
+					//    protocol: Request.Scheme);
 
-                    if (_userManager.Options.SignIn.RequireConfirmedAccount)
+					//await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+					//    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+					if (_userManager.Options.SignIn.RequireConfirmedAccount)
                     {
                         return RedirectToPage("RegisterConfirmation", new { email = Input.Email, returnUrl = returnUrl });
                     }
