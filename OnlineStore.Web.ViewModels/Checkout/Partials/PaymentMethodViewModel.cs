939e4ce95d774407ca72f1f8b4c48e617939c54e
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-07-21 16:27:21 +0300

    Improving the _PaymentSection view, so it is using the App PaymentMethodCode enums.Also modify the PaymentMethodViewModel so it is working with PaymentMethodCode enum.

--- OnlineStore.Web.ViewModels/Checkout/Partials/PaymentMethodViewModel.cs
+++ OnlineStore.Web.ViewModels/Checkout/Partials/PaymentMethodViewModel.cs
@@ -1,18 +1,13 @@
-﻿namespace OnlineStore.Web.ViewModels.Checkout.Partials
+﻿using OnlineStore.Data.Models.Enums;
+
+namespace OnlineStore.Web.ViewModels.Checkout.Partials
 {
 	public class PaymentMethodViewModel
 	{
 
-		public PaymentOption SelectedPaymentOption { get; set; }
+		public PaymentMethodCode SelectedPaymentOption { get; set; }
 
 		public CreditCardFormViewModel CreditCardDetails { get; set; } = new();
 
-		public enum PaymentOption
-		{
-			CreditCard = 0,
-			PayPal = 1,
-			GooglePay = 4,
-			CashOnDelivery = 5
-		}
 	}
 }
