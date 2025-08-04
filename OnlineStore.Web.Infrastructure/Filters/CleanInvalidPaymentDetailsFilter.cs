ae14e80e96c150857bbe25435f7ba5efa81985ae
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-08-04 18:08:09 +0300

    Creating the CleanInvalidPaymentDetailsFilter in order to remove it from the ModelState Validation, if the payment details are invalid or a different payment option other than the credit card payment option is selected.

---
+++ OnlineStore.Web.Infrastructure/Filters/CleanInvalidPaymentDetailsFilter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using OnlineStore.Data.Models.Enums;
+using OnlineStore.Web.ViewModels.Checkout;
+using OnlineStore.Web.ViewModels.Checkout.Partials;
+
+namespace OnlineStore.Web.Infrastructure.Filters
+{
+	public class CleanInvalidPaymentDetailsFilter : IActionFilter
+	{
+		private const string PaymentDetailsKey = "CreditCardDetails";
+
+		public void OnActionExecuting(ActionExecutingContext context)
+		{
+			if (context.ActionArguments.TryGetValue("model", out var obj)
+					&& obj is CheckoutViewModel model)
+			{
+				var modelState = context.ModelState;
+
+				if (model.Payment != null)
+				{
+					PaymentMethodViewModel payment = model.Payment;
+					PaymentMethodCode paymentMethodCode = payment.SelectedPaymentOption;
+
+					if ((payment.CreditCardDetails != null) && paymentMethodCode != PaymentMethodCode.CreditCard)
+					{
+						payment.CreditCardDetails = null;
+
+						foreach (var key in modelState.Keys.Where(k => k.Contains(PaymentDetailsKey)).ToList())
+						{
+							modelState.Remove(key);
+						}
+					}
+				}
+			}
+		}
+
+		public void OnActionExecuted(ActionExecutedContext context)
+		{
+		}
+	}
+}
