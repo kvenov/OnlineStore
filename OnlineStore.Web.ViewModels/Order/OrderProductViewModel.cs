7396ac0e0bf80aee63725494887621db5be28dfb
KonstantinIvanov <kokoivanov2008@gmail.com>
2025-08-06 18:19:39 +0300

    Including ProductSize into the ThankYou View.

--- OnlineStore.Web.ViewModels/Order/OrderProductViewModel.cs
+++ OnlineStore.Web.ViewModels/Order/OrderProductViewModel.cs
@@ -5,6 +5,7 @@
 		public string Name { get; set; } = null!;
 		public int Quantity { get; set; }
 		public decimal Price { get; set; }
+		public string ProductSize { get; set; } = null!;
 		public string ImageUrl { get; set; } = null!;
 	}
 }
